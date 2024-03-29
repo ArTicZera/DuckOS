549cc6e16f89c97b34b71c84f49425e5cb1b5722
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-29 18:41:11 -0300

    Update stdint.h

--- Include/stdint.h
+++ Include/stdint.h
@@ -1,1 +1,19 @@
+typedef unsigned char  uint8_t,  BYTE, UCHAR;
+typedef unsigned short uint16_t, WORD, UWORD;
+typedef unsigned int   uint32_t, DWORD, UINT;
+typedef unsigned long  ULONG;
 
+typedef char   CHAR;
+typedef short  SHORT;
+typedef int    INT;
+typedef float  FLOAT;
+typedef double DOUBLE;
+typedef long   LONG;
+
+typedef BYTE*  LPBYTE;
+typedef WORD*  LPWORD;
+typedef DWORD* LPDWORD;
+
+#define NULL  ((void*)0)
+#define TRUE  1
+#define FALSE 0
