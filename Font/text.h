157f87fa64733ef46c1e9e24eacefb8265f52511
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-29 18:40:23 -0300

    Update text.h

--- Font/text.h
+++ Font/text.h
@@ -1,1 +1,42 @@
+int cursorX = 0;
+int cursorY = 0;
 
+void DrawChar(BYTE* c, BYTE color)
+{
+    int i = 0;
+
+    for (int y = 0; y < HFONT; y++)
+    {
+        for (int x = WFONT - 1; x >= 0; x--)
+        {
+            if (c[y] & (1 << x))
+            {
+                SetPixel(i + cursorX, y + cursorY, color);
+            }
+            i++;
+        }
+        i = 0;
+    }
+
+    cursorX += 8;
+
+    if (cursorX >= 8 * 40)
+    {
+        cursorX = 0;
+        cursorY += 9;
+    }
+}
+
+void PrintString(const CHAR* str, BYTE color)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == '\n')
+        {
+            cursorX = -8;
+            cursorY += 9;
+        }
+
+        DrawChar(ProggyCleanTT+(str[i]*8), color);
+    }
+}
