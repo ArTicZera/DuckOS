290758b58b277de03f1e59744b94eb9de3069766
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-29 18:41:37 -0300

    Update kernel.cpp

--- Kernel/kernel.cpp
+++ Kernel/kernel.cpp
@@ -1,1 +1,9 @@
+#include "../Include/stdint.h"
+#include "../Graphics/graphics.h"
+#include "../Font/ProggyCleanTT.h"
+#include "../Font/text.h"
 
+void main(void)
+{
+    PrintString("Hello World!", 0x0F);
+}
