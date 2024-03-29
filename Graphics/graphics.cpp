18f6393354d385e3528418c62c285de93f2fd179
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-29 18:40:51 -0300

    Update graphics.cpp

--- Graphics/graphics.cpp
+++ Graphics/graphics.cpp
@@ -1,1 +1,10 @@
+#include "../Include/stdint.h"
 
+#include "graphics.h"
+
+void SetPixel(int x, int y, BYTE color)
+{
+    LPBYTE vidmem = (LPBYTE) 0xA0000;
+    
+    vidmem[y * WSCREEN + x] = color;
+}
