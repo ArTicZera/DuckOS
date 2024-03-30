3c00931f1ec2db7174b0fba1b2d4f406363884e4
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-30 12:40:52 -0300

    Update mem.cpp

--- Memory/mem.cpp
+++ Memory/mem.cpp
@@ -1,1 +1,15 @@
+#include "../Include/stdint.h"
 
+#include "mem.h"
+
+void* memset(void* dst, char val, DWORD num)
+{
+    LPBYTE tmp = (LPBYTE) dst;
+
+    for (DWORD i = 0; i < num; i++)
+    {
+        tmp[i] = (BYTE) val;
+    }
+
+    return dst;
+}
