46a46985a2a7c105e4a296dec32620d58ab50dd1
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-30 12:39:53 -0300

    Update idt.cpp

--- Interrupts/idt.cpp
+++ Interrupts/idt.cpp
@@ -1,1 +1,31 @@
+#include "../Include/stdint.h"
+#include "../Memory/mem.h"
 
+#include "idt.h"
+
+extern "C" void LoadIDT();
+
+struct IDT_GateDescriptor idt[TOTALGATES];
+struct IDT_Descriptor idtr;
+
+void SetupIDTGate(BYTE entry, DWORD isr, WORD selector, BYTE flags)
+{
+    idt[entry].selector = selector;
+    idt[entry].reserved = 0x00;
+    idt[entry].flags = flags;
+
+    idt[entry].offset  = isr & 0xFFFF; //Low 16 bits
+    idt[entry].offset2 = (isr >> 16) & 0xFFFF; //High 16 bits
+}
+
+void SetupIDT(void)
+{
+    int IDTSize = sizeof(struct IDT_GateDescriptor) * TOTALGATES;
+
+    idtr.size   = IDTSize - 1;
+    idtr.offset = (DWORD) &idt;
+
+    memset(&idt, 0, IDTSize);
+
+    LoadIDT();
+}
