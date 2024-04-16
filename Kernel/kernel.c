cd781d01a0217f250815710e9850005221088b31
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-16 17:16:00 -0300

    Update kernel.c

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,15 +1,18 @@
 #include "../Include/stdint.h"
 #include "../Graphics/graphics.h"
 #include "../Font/text.h"
-
+#include "../Drivers/keyboard.h"
 #include "../Interrupts/idt.h"
 
 void main(void)
 {
-    Debug("Kernel loaded!\n", 0);
+    Debug("Kernel Loaded!\n", 0);
 
     SetupIDT();
-    Debug("IDT Loaded!", 0);
+    Debug("IDT Loaded!\n", 0);
+
+    InitKeyboard();
+    Debug("Keyboard Driver Started!\n", 0);
 
     int a = 1 / 0;
 }
