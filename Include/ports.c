e2dcd01a7a1752be52bb31619c1cf78b76958011
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-16 17:12:35 -0300

    Update ports.c

--- Include/ports.c
+++ Include/ports.c
@@ -4,3 +4,10 @@ void outb(WORD Port, BYTE Value)
 {
     asm volatile ("outb %1, %0" : : "dN" (Port), "a" (Value));
 }
+
+char inb(WORD port)
+{
+    char ret;
+    asm volatile("inb %1, %0" : "=a" (ret) : "dN" (port));   
+    return ret;
+}
