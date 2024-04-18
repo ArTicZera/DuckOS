918aa9e819ac5fcb7f9c11885381eecb1f3aaf37
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-18 15:20:59 -0300

    Delete Font directory

--- Font/text.h
+++
@@ -1,5 +0,0 @@
-void DrawChar(BYTE* bitmap, BYTE color);
-void Print(const char* str, BYTE color);
-void Debug(const CHAR* str, int debug);
-void IntToString(int value, char* buffer);
-void PrintInt(int value, BYTE color);
