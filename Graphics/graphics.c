6dc5c8ef220a8a35e6de18524f2dc9a9e8ac5f17
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-20 14:59:01 -0300

    Update graphics.c

--- Graphics/graphics.c
+++ Graphics/graphics.c
@@ -8,3 +8,10 @@ void SetPixel(int x, int y, BYTE color)
     
     vidmem[y * WSCREEN + x] = color;
 }
+
+BYTE GetPixel(int x, int y)
+{
+    LPBYTE vidmem = (LPBYTE) VIDEOMEM;
+
+    return vidmem[y * WSCREEN + x];
+}
